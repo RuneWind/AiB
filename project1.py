4d81a2981f2bfad924706cbaa7fcf7dba9cfb28a
Astrid Christiansen <astridpigen2@gmail.com>
2020-02-06 11:50:29 +0100

    backtracking works for small seqs

--- project1.py
+++ project1.py
@@ -20,11 +20,11 @@ gap_cost = -5
 #Calculate cost of an optimal alignment for string str_A and str_B with substitution matrix sm and gap cost gc
 def calculate_alignment_matrix(sm, gc, str_A, str_B):
     #create array with None values
-    T = np.full((len(str_A), len(str_B)), None)
+    T = np.full((len(str_A) + 1, len(str_B) + 1), None)
     #iterate through rows
-    for i in range(0, len(str_A)):
+    for i in range(0, len(str_A) + 1):
         #iterate through columns
-        for j in range(0, len(str_B)):
+        for j in range(0, len(str_B) + 1):
             T[i,j] = calc_cost(i, j, T, str_A, str_B, sm, gc)
     return T
 
@@ -34,7 +34,7 @@ def calc_cost(i, j, T, str_A, str_B, sm, gc):
         cost = float("-inf")
         #get diagonal value
         if(i > 0 and j > 0):
-            cost = calc_cost(i-1, j-1, T, str_A, str_B, sm, gc) + sm[str_A[i]][str_B[j]]
+            cost = calc_cost(i-1, j-1, T, str_A, str_B, sm, gc) + sm[str_A[i-1]][str_B[j-1]]
         #get above value
         if(i > 0 and j >= 0):
             cost = max(cost, calc_cost(i-1, j, T, str_A, str_B, sm, gc) + gc)
@@ -52,21 +52,21 @@ def calc_cost(i, j, T, str_A, str_B, sm, gc):
 #last cell: T[len(str_A) - 1, len(str_B) - 1]
 def backtrack(T, str_A, str_B, sm, gc, res_str_A, res_str_B, i, j):
     cell = T[i, j]
+    print("i: " + str(i) + " j:" + str(j))
     #diagonal cell - substitution
-    if (i > 0 and j > 0 and cell == T[i-1, j-1] + sm[str_A[i]][str_B[j]]):
-        backtrack(T, str_A, str_B, sm, gc, res_str_A + str_A[i] , res_str_B + str_B[j], i-1, j-1)
+    if (i > 0 and j > 0 and cell == T[i-1, j-1] + sm[str_A[i-1]][str_B[j-1]]):
+        backtrack(T, str_A, str_B, sm, gc, res_str_A + str_A[i-1] , res_str_B + str_B[j-1], i-1, j-1)
     #upper cell - insertion    
     elif (i > 0 and j >= 0 and cell == T[i-1, j] + gc):
-        backtrack(T, str_A, str_B, sm, gc, res_str_A + "-", res_str_B + str_B[j],  i-1, j)
+        backtrack(T, str_A, str_B, sm, gc, res_str_A + str_A[i-1], res_str_B + "-",  i-1, j)
     #left cell - deletion
     elif (i >= 0 and j > 0 and cell == T[i, j-1]):
-        backtrack(T, str_A, str_B, sm, gc, res_str_A + str_A[j], res_str_B + "-", i, j-1)
+        backtrack(T, str_A, str_B, sm, gc, res_str_A + "-", res_str_B + str_B[j-1], i, j-1)
     elif (i==0 and j==0):
-        print("HALLOOOOO!")
-        print(res_str_A + "\n" + res_str_B)
-        #x = open("alignment.fasta", "w")
-        #x.write(res_str_A + "/n" + res_str_B)
-        #x.close()
+        print(res_str_A[::-1] + "\n" + res_str_B[::-1])
+        x = open("alignment.fasta", "w")
+        x.write(res_str_A[::-1] + "\n" + res_str_B[::-1])
+        x.close()
 
         
         
@@ -76,6 +76,7 @@ def backtrack(T, str_A, str_B, sm, gc, res_str_A, res_str_B, i, j):
 string_A = "AATAAT"
 string_B = "AAGG"
 t = calculate_alignment_matrix(sub_matrix, gap_cost, string_A, string_B)
+#print(t)
 #print(t[len(string_A) - 1, len(string_B) - 1])
 #Optimal alignment: 10
 
@@ -84,12 +85,13 @@ t = calculate_alignment_matrix(sub_matrix, gap_cost, string_A, string_B)
 fasta1 = read_fasta_file("seq1.fasta")
 fasta2 = read_fasta_file("seq2.fasta")
 t2 = calculate_alignment_matrix(sub_matrix, gap_cost, fasta1.seq, fasta2.seq)
+#print(t2)
 #print(t2[len(fasta1.seq) - 1, len(fasta1.seq) -1])
 #Optimal alignment: 1336
 
 # Question 3
-backtrack(t, string_A, string_B, sub_matrix, gap_cost, "", "", len(string_A) - 1, len(string_B) - 1)
-#backtrack(t2, fasta1.seq, fasta2.seq, sub_matrix, gap_cost, "", "", len(fasta1.seq) - 1, len(fasta2.seq) - 1)
+#backtrack(t, string_A, string_B, sub_matrix, gap_cost, "", "", len(string_A), len(string_B))
+backtrack(t2, fasta1.seq, fasta2.seq, sub_matrix, gap_cost, "", "", len(fasta1.seq), len(fasta2.seq))
 
 
  
