c3734c7f3809e1a7ca56f44a138df95d5fa1ae55
dtorlyn <60732752+dtorlyn@users.noreply.github.com>
2020-02-21 13:34:32 +0100

    Update project2_affine.py
    affine gap cost

--- project2_affine.py
+++ project2_affine.py
@@ -72,34 +72,51 @@ gap_cost = -5
 
 
 #Calculate cost of an optimal alignment for string str_A and str_B with substitution matrix sm and gap cost gc
-def calculate_alignment_matrix(sm, gc, str_A, str_B):
+def calculate_alignment_matrix(sm, gc_a, gc_b, str_A, str_B):
     #create array with None values to fill with cost values
     T = np.full((len(str_A) + 1, len(str_B) + 1), None)
     #create array with None values to fill with # of predecessors values    
     P = np.full((len(str_A) + 1, len(str_B) + 1), None)
 
+    D = np.full((len(str_A) + 1, len(str_B) + 1), None)
+    I = np.full((len(str_A) + 1, len(str_B) + 1), None)
+
     #iterate through rows
     for i in range(0, len(str_A) + 1):
         #iterate through columns
         for j in range(0, len(str_B) + 1):
-            T[i,j], P[i,j] = calc_cost(i, j, T, P, str_A, str_B, sm, gc)
+            T[i,j], P[i,j], D[i,j], I[i,j] = calc_cost(i, j, T, P, D, I, str_A, str_B, sm, gc_a, gc_b)
     print(T[len(str_A),len(str_B)])
     return T, P
 
 #Calculate cost of one cell
-def calc_cost(i, j, T, P, str_A, str_B, sm, gc):
+def calc_cost(i, j, T, P, D, I, str_A, str_B, sm, gc_a, gc_b):
     if(T[i,j] is None):
         pred = 0
         diag_cost = above_cost = left_cost = zero_cost = float("inf")
+        diag_cost_D = diag_cost_I = above_cost_D = left_cost_I = float("inf")
+        if(D[i,j] is None):
+        	if(i > 0 and j >= 0):
+        		diag_cost_D = T[i-1,j] + (gc_a + gc_b)
+        	if (i > 1 and j >=0):
+        		above_cost_D = D[i-1,j] + gc_a
+        	deletion = min(diag_cost_D, above_cost_D)
+        if(I[i,j] is None):
+        	if(i >= 0 and j > 0):
+        		diag_cost_I = T[i,j-1] + (gc_a + gc_b)
+        	if (i >=0  and j > 1):
+        		left_cost_I = D[i,j-1] + gc_a
+        	insertion = min(diag_cost_I, left_cost_I)
+
         #get diagonal value
         if(i > 0 and j > 0):
-            diag_cost = calc_cost(i-1, j-1, T, P, str_A, str_B, sm, gc) + sm[str_A[i-1]][str_B[j-1]]
+            diag_cost = calc_cost(i-1, j-1, T, P, D, I, str_A, str_B, sm, gc_a, gc_b) + sm[str_A[i-1]][str_B[j-1]]
         #get above value
         if(i > 0 and j >= 0):
-            above_cost = calc_cost(i-1, j, T, P, str_A, str_B, sm, gc) + gc
+            above_cost = deletion
         #get left value
         if(i >= 0 and j > 0):
-            left_cost = calc_cost(i, j-1, T, P, str_A, str_B, sm, gc) + gc
+            left_cost = insertion
         #Left top corner
         if(i == 0 and j == 0):
             zero_cost = 0
@@ -113,7 +130,7 @@ def calc_cost(i, j, T, P, str_A, str_B, sm, gc):
             pred += P[i-1,j] 
         if(left_cost == min_val):
             pred += P[i,j-1] 
-        return min_val, pred  
+        return min_val, pred, deletion, insertion 
     return T[i,j]
 
 
@@ -121,7 +138,7 @@ def calc_cost(i, j, T, P, str_A, str_B, sm, gc):
 
 #Find an optimal alignment based on an alignment matrix, T
 #last cell: T[len(str_A) - 1, len(str_B) - 1]
-def backtrack(T, str_A, str_B, sm, gc, res_str_A, res_str_B, i, j):
+def backtrack(T, str_A, str_B, sm, gc_a, gc_b, res_str_A, res_str_B, i, j):
     cell = T[i, j]
     #print("i: " + str(i) + " j:" + str(j))
     #diagonal cell - substitution
@@ -179,16 +196,17 @@ def backtrack(T, str_A, str_B, sm, gc, res_str_A, res_str_B, i, j):
 #we get 4 optimal alignments
 
 sub_matrix = parse_phylip(sys.argv[1])
-gap_cost = int(sys.argv[2])
-str_A = read_fasta_file(sys.argv[3]).seq.upper()
-str_B = read_fasta_file(sys.argv[4]).seq.upper()
+gap_cost_a = int(sys.argv[2])
+gap_cost_b = int(sys.argv[3])
+str_A = read_fasta_file(sys.argv[4]).seq.upper()
+str_B = read_fasta_file(sys.argv[5]).seq.upper()
 
 
-t3, p3 = calculate_alignment_matrix(sub_matrix, gap_cost, str_A, str_B)
+t3, p3 = calculate_alignment_matrix(sub_matrix, gap_cost_a, gap_cost_b, str_A, str_B)
 print(sys.argv)
-if len(sys.argv)==6:
-	if sys.argv[5]=="True":
-		b = backtrack(t3, str_A, str_B, sub_matrix, gap_cost, "", "", len(str_A), len(str_B))
+if len(sys.argv)==7:
+	if sys.argv[6]=="True":
+		b = backtrack(t3, str_A, str_B, sub_matrix, gap_cost_a, gap_cost_b, "", "", len(str_A), len(str_B))
 	
 
 
