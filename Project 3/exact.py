5b5cbcb025cf8cf7f2feac8d30ddc3bd101a457e
dtorlyn <60732752+dtorlyn@users.noreply.github.com>
2020-03-11 14:23:13 +0100

    input format for exact algo

--- Project 3/exact.py
+++ Project 3/exact.py
@@ -1,4 +1,6 @@
 import numpy as np
+from Bio import SeqIO
+import sys 
 
 
 sub_m = {"A": {"A": 0, "C": 5, "G": 2, "T": 5}, 
@@ -6,6 +8,64 @@ sub_m = {"A": {"A": 0, "C": 5, "G": 2, "T": 5},
             "G": {"A": 2, "C": 5, "G": 0, "T": 5}, 
             "T": {"A": 5, "C": 2, "G": 5, "T": 0}}
 gc = 5
+
+
+# Read fasta files
+def read_fasta_file(filename):
+    rec_list = []
+    nucleic_list = ["U", "W", "S", "M", "K", "R", "Y", "B", "D", "H", "V", "N", "Z"]
+    for record in SeqIO.parse(filename, "fasta"):
+        corrected_seq = str(record.seq)
+        for symbol in nucleic_list:
+            corrected_seq = corrected_seq.replace(symbol, "A")
+        rec_list.append(corrected_seq)
+    return rec_list
+
+"""
+HELPER FUNCTION
+Reads a file of this format:
+4 
+A  10  2  5  2
+C  2  10  2  5
+G  5  2  10  2
+T  2  5  2  10
+
+representing a substitution matrix and returns a dictionary corresponding to the substitutionmatrix
+
+Returning a dictionary of this format (if getAlphabet = False):
+{"A": {"A": 10, "C": 2, "G": 5, "T": 2}, 
+ "C": {"A": 2, "C": 10, "G": 2, "T": 5}, 
+ "G": {"A": 5, "C": 2, "G": 10, "T": 2}, 
+ "T": {"A": 2, "C": 5, "G": 2, "T": 10}}
+
+If getAlphabet = True, we instead return a list of the alphabet letters:
+['A', 'C', 'G', 'T']
+"""
+def parse_phylip(filename, getAlphabet = False):
+    f= open(filename, "r")    
+    f1 = f.readlines()
+    f2 = list()
+    for x in f1:
+        f2.append(x.split())
+    alph_size = int(f2[0][0])
+    
+    letters = list()
+    for i in range(1, alph_size+1):
+        letters.insert(i, f2[i][0])
+    
+    sub_matrix = dict()
+    for i in range(len(letters)):
+        inner_dict = dict()
+        for j in range(len(letters)):
+            inner_dict[letters[j]] = int(f2[i+1][j+1])
+        sub_matrix[letters[i]] = inner_dict
+    if(getAlphabet):
+        return letters
+    else:
+        return sub_matrix
+
+
+
 
 #Calculate cost of an optimal alignment for string str_A and str_B with substitution matrix sm and gap cost gc
 def calculate_alignment_matrix(sub_m, gap_cost, strA, strB, strC):
@@ -62,11 +122,14 @@ def calc_cost_nonrec(i, j, k):
     else:
         return T[i,j,k]
 
-def backtrack_nonrec(T, str_A, str_B, str_C, i, j, k):
+def backtrack_nonrec(T, str_A, str_B, str_C):
     print("Backtracking nonrecursively")
     res_str_A = ""    
     res_str_B = ""
     res_str_C = ""
+    i = len(str_A)
+    j = len(str_B)
+    k = len(str_C)
     while(i >= 0 and j >= 0 and k>= 0):
         cell = T[i, j, k]
         #diagonal cell - substitution
@@ -122,11 +185,50 @@ def backtrack_nonrec(T, str_A, str_B, str_C, i, j, k):
             return [res_str_A[::-1], res_str_B[::-1], res_str_C[::-1]]
 
 
+def print_alignment_to_file(seq_list):
+    print("Printing alignment to file...")
+    #write alignment list to fasta file
+    x = open("alignment.fasta", "w")
+    for i in range(len(seq_list)):    
+        x.write(">seq" + str(i+1) + "\n" + seq_list[i] + "\n")
+    x.close()
+
+
+
+
+
+# Code we run from command line
+# Get sub matrix, gap cost, and sequences from command line variables
+sub_matrix = parse_phylip(sys.argv[1])
+gap_cost = int(sys.argv[2])
+str_A = read_fasta_file(sys.argv[3])[0]
+str_B = read_fasta_file(sys.argv[3])[1]
+str_C = read_fasta_file(sys.argv[3])[2]
+
+# Get letters specified in substitution matrix file
+letters = parse_phylip(sys.argv[1], True)
+
+# Check if sequences only contain allowed letters 
+if(all(c in letters for c in str_A) and all(c in letters for c in str_B) and all(c in letters for c in str_C)):
+    # Calculate alignment matrix and print optimal cost
+    t = calculate_alignment_matrix(sub_matrix, gap_cost, str_A, str_B, str_C)
+    # If we want to backtrack, write optimal alignment in file alignment.fasta
+    if len(sys.argv)==5 and sys.argv[4]=="True":
+        b = backtrack_nonrec(t, str_A, str_B, str_C)
+        print_alignment_to_file(b)
+
+else:
+    print("Error: A letter in a sequence is not specified in the substitution matrix.")
+   
+
+
+
+
 '''
 strA = "GTTCCGAAAGGCTAGCGCTAGGCGCCAAGCGGCCGGTTTCCTTGGCGACGGAGAGCGCGGGAATTTTAGATAGATTGTAATTGCGGCTGCGCGGCCGCTGCCCGTGCAGCCAGAGGATCCAGCACCTCTCTTGGGGCTTCTCCGTCCTCGGCGCTTGGAAGTACGGATCTTTTTTCTCGGAGAAAAGTTCACTGGAACTG"
 strB = "ATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAACGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGA"
 strC = "CGCTGGTGCAACTCGAAGACCTATCTCCTTCCCGGGGGGGCTTCTCCGGCATTTAGGCCTCGGCGTTTGGAAGTACGGAGGTTTTTCTCGGAAGAAAGTTCACTGGAAGTGGAAGAAATGGATTTATCTGCTGTTCGAATTCAAGAAGTACAAAATGTCCTTCATGCTATGCAGAAAATCTTGGAGTGTCCAATCTGTTT"
-'''
+
 
 strA = "GTTCCGAAAGGCTAGCGCTAGGCGCC"
 strB = "ATGGATTTATCTGCTCTTCG"
@@ -137,3 +239,6 @@ print(D[len(strA), len(strB), len(strC)])
 
 b = backtrack_nonrec(T, strA, strB, strC, len(strA), len(strB), len(strC))
 print(b)
+
+print_alignment_to_file(b)
+'''
