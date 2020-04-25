4c4faaf9c2f413b07ea0de2a3758cd4be6c7cd03
Allegris <AstridChristiansenCS@gmail.com>
2020-04-25 06:58:41 +0200

    File structure and bug fix

--- Project5/nj.py
+++ Project5/nj.py
@@ -1,16 +1,19 @@
 import numpy as np
+from Bio import Phylo
 
 
 """
 HELPER FUNCTION
 Reads a file of this format:
-4 
-A  10  2  5  2
-C  2  10  2  5
-G  5  2  10  2
-T  2  5  2  10
+5
+A 0.00 0.23 0.16 0.20 0.17
+B 0.23 0.00 0.23 0.17 0.24
+C 0.16 0.23 0.00 0.20 0.11
+D 0.20 0.17 0.20 0.00 0.21
+E 0.17 0.24 0.11 0.21 0.00
 
-representing a substitution matrix and returns a dictionary corresponding to the substitutionmatrix
+representing a dissimilarity/distance matrix and returns a numpy array of the matrix
+and a dict for translating letters to indices
 
 Returning a dictionary of this format (if getAlphabet = False):
 {"A": {"A": 10, "C": 2, "G": 5, "T": 2}, 
@@ -18,10 +21,9 @@ Returning a dictionary of this format (if getAlphabet = False):
  "G": {"A": 5, "C": 2, "G": 10, "T": 2}, 
  "T": {"A": 2, "C": 5, "G": 2, "T": 10}}
 
-If getAlphabet = True, we instead return a list of the alphabet letters:
-['A', 'C', 'G', 'T']
+If getAlphabet = True, we instead return a list of the node names:
+['A', 'B', 'C', 'D', 'E']
 """
-
 def parse_phylip(filename, getAlphabet = False):
     f= open(filename, "r")    
     f1 = f.readlines()
@@ -33,14 +35,7 @@ def parse_phylip(filename, getAlphabet = False):
     letters = list()
     for i in range(1, alph_size+1):
         letters.insert(i, f2[i][0])
-    '''
-    sub_dict = dict()
-    for i in range(len(letters)):
-        inner_dict = dict()
-        for j in range(len(letters)):
-            inner_dict[letters[j]] = f2[i+1][j+1]
-        sub_dict[letters[i]] = inner_dict
-    '''
+        
     dist_matrix = np.zeros((len(letters), len(letters)))  
     for i in range(len(letters)):
         inner_list = np.zeros(len(letters))
@@ -55,21 +50,115 @@ def parse_phylip(filename, getAlphabet = False):
         return letters
     else:
         return dist_matrix, index_to_letter_dict
+
+
+def print_tree_to_file(tree):
+    x = open("tree.newick", "w")
+    x.write(tree)
+    x.close()    
     
- 
+    
+def get_d_and_rs(dist_matrix, S, i, j):
+    d_ij = dist_matrix[i][j]
+    r_i = 1/(len(S)-2) * sum([dist_matrix[i][m] for m in S])
+    r_j = 1/(len(S)-2) * sum([dist_matrix[j][m] for m in S])
+    return d_ij, r_i, r_j
+    
+    
 def nj(dist_matrix, index_to_letter_dict):
-    S = index_to_letter_dict.values()
+    S_nodes = list(index_to_letter_dict.values())
+    S = index_to_letter_dict.keys()
     while len(S) > 3:
-        min_val = dist_matrix.min()
-        return min_val
+        
+        # Step 1 (a): Compute matrix N
+        # Matrix N entry (i,j) is a number indicating "how cloase i and j are to each other and how far they are from the rest"
+        N = np.zeros((len(S), len(S)))
+        for i in range(len(S)):
+            inner_list = dist_matrix[i]
+            for j in range(len(inner_list)):
+                d_ij, r_i, r_j = get_d_and_rs(dist_matrix, S, i, j)
+                N[i][j] = d_ij - (r_i + r_j)
+        
+        # Step 1 (b): Find minimum entry in matrix N
+        N_removed_diag = [N[i][j] for i in S for j in S if i != j]
+        min_val = min(N_removed_diag)
+        min_i_j = np.where(N == min_val)
+        listOfCordinates = list(zip(min_i_j[0], min_i_j[1]))
+        min_coord = None
+        for coord in listOfCordinates:
+            if(coord[0] != coord[1]):
+                min_coord = coord
+                break
+        i = int(min_coord[0])
+        j = int(min_coord[1])
+        
+        # Step 2 and 3: Add new node k to the tree with weights
+        d_ij, r_i, r_j = get_d_and_rs(dist_matrix, S, i, j)
+        weight_ki = (1/2) * (d_ij + r_i - r_j)
+        weight_kj = (1/2) * (d_ij + r_j - r_i) #d_ij - weight_ki equals (1/2) * (d_ij + r_j - r_i)
+        leaf_1 = S_nodes[i]
+        leaf_2 = S_nodes[j]
+        k = "(" + leaf_1 + ":" + str(round(weight_ki, 3)) + ", " + leaf_2 + ":" + str(round(weight_kj, 3)) + ")"
+        
+        
+        # Step 4: Update dist_matrix (delete rows i and j and columns i and j and add new row and column for node k)
+        
+        # Create row for new node k to insert
+        row = []
+        for m in range(len(dist_matrix)):
+            if(m != i and m != j):
+                d_km = (1/2) * (dist_matrix[i][m] + dist_matrix[j][m] - d_ij)
+                row.append(d_km)        
+        
+        # Remove row i and j
+        dist_matrix = np.delete(dist_matrix, [i,j], 0)        
+        # Remove column i and j
+        dist_matrix = np.delete(dist_matrix, [i,j], 1)        
+
+        # Insert row and column for new node k
+        dist_matrix = np.row_stack((dist_matrix, row))
+        dist_matrix = np.column_stack((dist_matrix, np.append(row, 0)))
+        
+        # Step 5: Delete i and j from S and add new node k to S
+        S_nodes.remove(leaf_1)
+        S_nodes.remove(leaf_2)
+        S_nodes.append(k)
+        S = list(range(len(S_nodes)))
+        
+    # Termination: We now have 3 leaves left: i, j and m
+    # Add a new node v to the tree with weights
+    i = S[0]
+    j = S[1]
+    m = S[2]
+    weight_vi = (1/2) * (dist_matrix[i][j] + dist_matrix[i][m] - dist_matrix[j][m])        
+    weight_vj = (1/2) * (dist_matrix[i][j] + dist_matrix[j][m] - dist_matrix[i][m])        
+    weight_vm = (1/2) * (dist_matrix[i][m] + dist_matrix[j][m] - dist_matrix[i][j])       
+
+    v = "(" + S_nodes[i] + ":" + str(round(weight_vi, 3)) + ", " + S_nodes[j] + ":" + str(round(weight_vj, 3)) + ", " + S_nodes[m] + ":" + str(round(weight_vm, 3)) + ");" 
     
-#print(parse_phylip("example_slide4.phy"))
+    return v
+
+
+
+'''
+Code to run
+'''        
+# Read distance matrix
+'''
 distance_matrix, index_to_letter_dict = parse_phylip("example_slide4.phy")
 letters = parse_phylip("example_slide4.phy", True)
+'''
+distance_matrix, index_to_letter_dict = parse_phylip("89_Adeno_E3_CR1.phy")
+letters = parse_phylip("89_Adeno_E3_CR1.phy", True)
 
 
+# Construct tree
+tree = nj(distance_matrix, index_to_letter_dict) 
+print_tree_to_file(tree)
 
-print(nj(distance_matrix, index_to_letter_dict))
+# Draw tree
+tree1 = Phylo.read("tree.newick", 'newick')
+#Phylo.draw(tree1, branch_labels=lambda c: c.branch_length)
 
 
 
