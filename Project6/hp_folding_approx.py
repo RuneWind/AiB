e4c23f34213f3355dfaf383f78c29b85299d7295
RuneWind <50365450+RuneWind@users.noreply.github.com>
2020-05-06 09:11:34 +0200

    Folding string
    ..

--- Project6/hp_folding_approx.py
+++ Project6/hp_folding_approx.py
@@ -28,22 +28,50 @@ def calculate_matches(hp_str):
         
     # Return tuples of matchings in list from the largest number of matchings
     if even_odd_score > odd_even_score:
-        return zip(even_s[0: even_odd_score], odd_s_rev[0: even_odd_score])
+        return list(zip(even_s[0: even_odd_score], odd_s_rev[0: even_odd_score]))
 
     if even_odd_score <= odd_even_score:
         return list(zip(odd_s[0: odd_even_score], even_s_rev[0: odd_even_score]))
 
 
-#def calculate_free_energy(match_indecies):
-	
+def calculate_free_energy(match_indecies, hp_str):
+	fold_str = ""
+	fold_str += match_indecies[0][0] * "e"
 
+	for i in range(0, len(match_indecies)-1):
+		if match_indecies[i+1][0] - match_indecies[i][0] > 2:
+			fold_str += "n" * ((match_indecies[i+1][0] - match_indecies[i][0] - 1) // 2)
+			fold_str += "e"
+			fold_str += "s" * ((match_indecies[i+1][0] - match_indecies[i][0] - 1) // 2)
+			fold_str += "e"
+
+		else:
+			fold_str += 2 * "e"
+	fold_str += ((match_indecies[len(match_indecies)-1][1] - match_indecies[len(match_indecies)-1][0] - 1) // 2) * "e"
+
+	fold_str += "s"
+	fold_str += ((match_indecies[len(match_indecies)-1][1] - match_indecies[len(match_indecies)-1][0] - 1) // 2) * "w"
+
+	for i in range(len(match_indecies)-1, 0, -1):
+		if match_indecies[i-1][1] - match_indecies[i][1] > 2:
+			fold_str += "s" * ((match_indecies[i][0] - match_indecies[i-1][0] - 1) // 2)
+			fold_str += "w"
+			fold_str += "n" * ((match_indecies[i][0] - match_indecies[i-1][0] - 1) // 2)
+			fold_str += "w"
+
+		else:
+			fold_str += 2 * "w"
+
+	fold_str += (len(hp_str) - match_indecies[0][1] - 1) * "w"
+	
+	print(fold_str)
 
 
 
 hp_str = "hhhhhhhhhhhhphphpphhpphhpphpphhpphhpphpphhpphhpphphphhhhhhhhhhhh"
 match_indecies  = calculate_matches(hp_str)
-print(match_indecies)
-#calculate_free_energy(match_indecies)
+#print(match_indecies)
+calculate_free_energy(match_indecies, hp_str)
 
 
 
