25acea3a79e768459b1da04d74f4d28e45860bc5
RuneWind <50365450+RuneWind@users.noreply.github.com>
2020-05-11 10:51:11 +0200

    Fixed bugs
    ..

--- Project6/hp_folding_approx.py
+++ Project6/hp_folding_approx.py
@@ -2,41 +2,41 @@ import os
 
 def calculate_matches(hp_str):
 
-    # Lists of even indices and odd indices of h's in input "hp"-string
-    even_s = [i for i in range(len(hp_str)) if i%2 == 0 and hp_str[i] == "h"]
-    odd_s = [i for i in range(len(hp_str)) if i%2 == 1 and hp_str[i] == "h"]
-    
-    # Lists in reverse lists
-    odd_s_rev = odd_s[::-1]
-    even_s_rev = even_s[::-1]
-    
-    # Number of matchings when taking the evens from left and the odds from right
-    even_odd_score = 0
-    # Number of matchings when taking the odds from left and the evens from right
-    odd_even_score = 0
+	# Lists of even indices and odd indices of h's in input "hp"-string
+	even_s = [i for i in range(len(hp_str)) if i%2 == 0 and hp_str[i] == "h"]
+	odd_s = [i for i in range(len(hp_str)) if i%2 == 1 and hp_str[i] == "h"]
+	
+	# Lists in reverse lists
+	odd_s_rev = odd_s[::-1]
+	even_s_rev = even_s[::-1]
+	
+	# Number of matchings when taking the evens from left and the odds from right
+	even_odd_score = 0
+	# Number of matchings when taking the odds from left and the evens from right
+	odd_even_score = 0
 	
 	# Match evens from left with odds from right, i.e. match evens with reverse odds
-    i = 0
-    while even_s[i] < odd_s_rev[i]:
-        even_odd_score += 1
-        i += 1
-        if i >= min(len(odd_s_rev), len(even_s)):
-        	break
+	i = 0
+	while even_s[i] < odd_s_rev[i]:
+		even_odd_score += 1
+		i += 1
+		if i >= min(len(odd_s_rev), len(even_s)):
+			break
 
 	# Match odds from left with evens from right, i.e. match odds with reverse evens
-    i = 0
-    while odd_s[i] < even_s_rev[i]:
-        odd_even_score += 1
-        i += 1
-        if i >= min(len(odd_s), len(even_s_rev)):
-        	break
-        
-    # Return tuples of matchings in list from the largest number of matchings
-    if even_odd_score > odd_even_score:
-        return list(zip(even_s[0: even_odd_score], odd_s_rev[0: even_odd_score]))
+	i = 0
+	while odd_s[i] < even_s_rev[i]:
+		odd_even_score += 1
+		i += 1
+		if i >= min(len(odd_s), len(even_s_rev)):
+			break
+		
+	# Return tuples of matchings in list from the largest number of matchings
+	if even_odd_score > odd_even_score:
+		return list(zip(even_s[0: even_odd_score], odd_s_rev[0: even_odd_score]))
 
-    if even_odd_score <= odd_even_score:
-        return list(zip(odd_s[0: odd_even_score], even_s_rev[0: odd_even_score]))
+	if even_odd_score <= odd_even_score:
+		return list(zip(odd_s[0: odd_even_score], even_s_rev[0: odd_even_score]))
 
 
 def create_fold_string(match_indecies, hp_str):
@@ -45,6 +45,12 @@ def create_fold_string(match_indecies, hp_str):
 
 	# Add e's corresponding to bases before first match
 	fold_str += match_indecies[0][0] * "e"
+
+	min_overhang = min(match_indecies[0][0], len(hp_str)-match_indecies[0][1]-1)
+	for i in range(1, min_overhang+1):
+		if hp_str[match_indecies[0][0]-i] == "h" and hp_str[match_indecies[0][1]+i] == "h":
+			free_energy += 1
+
 
 	# Make left to right portion of fold string
 	for i in range(0, len(match_indecies)-1):
@@ -86,9 +92,9 @@ def create_fold_string(match_indecies, hp_str):
 	for i in range(len(match_indecies)-1, 0, -1):
 		# in case of loop
 		if match_indecies[i-1][1] - match_indecies[i][1] > 2:
-			fold_str += "s" * ((match_indecies[i][0] - match_indecies[i-1][0] - 1) // 2)
+			fold_str += "s" * ((match_indecies[i-1][1] - match_indecies[i][1] - 1) // 2)
 			fold_str += "w"
-			fold_str += "n" * ((match_indecies[i][0] - match_indecies[i-1][0] - 1) // 2)
+			fold_str += "n" * ((match_indecies[i-1][1] - match_indecies[i][1] - 1) // 2)
 			fold_str += "w"
 			if hp_str[match_indecies[i-1][1]-1] == "h":
 				free_energy += 1
@@ -104,15 +110,24 @@ def create_fold_string(match_indecies, hp_str):
 
 
 
-hp_str = "hhppppphhppphppphp"
-match_indecies  = calculate_matches(hp_str)
+
+hp_strs = ["hhppppphhppphppphp", "hphphhhppphhhhpphh", "phpphphhhphhphhhhh", "hphpphhphpphphhpphph", "hhhpphphphpphphphpph", "hhpphpphpphpphpphpphpphh", "pphpphhpppphhpppphhpppphh", "ppphhpphhppppphhhhhhhpphhpppphhpphpp", "pphpphhpphhppppphhhhhhhhhhpppppphhpphhpphpphhhhh", "hhphphphphhhhphppphppphpppphppphppphphhhhphphphphh", "pphhhphhhhhhhhppphhhhhhhhhhphppphhhhhhhhhhhhpppphhhhhhphhphp", "hhhhhhhhhhhhphphpphhpphhpphpphhpphhpphpphhpphhpphphphhhhhhhhhhhh", "hhhhpppphhhhhhhhhhhhpppppphhhhhhhhhhhhppphhhhhhhhhhhhppphhhhhhhhhhhhppphpphhpphhpphph", "pppppphphhppppphhhphhhhhphhpppphhpphhphhhhhphhhhhhhhhhphhphhhhhhhppppppppppphhhhhhhpphphhhpppppphphh", "ppphhpphhhhpphhhphhphhphhhhpppppppphhhhhhpphhhhhhppppppppphphhphhhhhhhhhhhpphhhphhphpphphhhpppppphhh"]
+for seq in hp_strs:
+	print("\n\n", seq)
+	match_indecies = calculate_matches(seq)
+	print(match_indecies)
+	fold_string = create_fold_string(match_indecies, seq)
+	os.system("python hpview3k.py " + seq + " " + fold_string )
+	
+
+#match_indecies  = calculate_matches(hp_str)
+##print(match_indecies)
+#fold_string = create_fold_string(match_indecies, hp_str)
+#
+#print(len(hp_str), len(fold_string))
 #print(match_indecies)
-fold_string = create_fold_string(match_indecies, hp_str)
-
-print(len(hp_str), len(fold_string))
-print(match_indecies)
-print(fold_string)
-
-os.system("python hpview3k.py " + hp_str + " " + fold_string )
+#print(fold_string)
+#
+#os.system("python hpview3k.py " + hp_str + " " + fold_string )
 
 
